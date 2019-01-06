bf73a05176942f59b2be155c879af7c4c6417d0a
aryant shukla <aeyantshukla@gmail.com>
2019-01-06 15:18:44 +0530

    Create stockpriceextractor.py

---
+++ stockpriceextractor.py
@@ -0,0 +1,26 @@
+import re
+import urllib.request
+import urllib
+
+url = "https://www.google.com/finance?q="
+stock = input("Enter your stock: ")
+url = url + stock
+data= urllib.request.urlopen(url).read()
+data1 = data.decode("utf-8")
+
+req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"}) 
+data1 = urllib.request.urlopen( req )
+m = data1.read()
+
+m = m.decode("windows-1252")
+
+
+
+s = m.find('US$')
+start = int(s)
+end = start+10
+print(m[start:end])
+
+
+s = re.search('US$',m)
+print(s)
